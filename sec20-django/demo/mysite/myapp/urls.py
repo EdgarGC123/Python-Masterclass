321678954203650d949189e6fe6855f6d0c4d368
Edgar GC <Edgar.GalvanCuesta@gmail.com>
2021-03-12 17:35:33 -0600

    pushing yesterday's and today's work

---
+++ sec20-django/demo/mysite/myapp/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+
+app_name = 'myapp'
+urlpatterns = [
+    path('', views.index, name="index"),
+    # path('products/', views.products, name="products")# products is removed from my app views. 
+    #book/1 detail views
+    path('book/<int:book_id>/', views.detail, name='detail'),
+]
--- sec20-django/demo/mysite/myapp/views.py
+++ sec20-django/demo/mysite/myapp/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+from .models import Book #importing our books class
 # Create your views here.
+
+def index(request):
+    book_list = Book.objects.all() #getting all the list of books
+    context = {
+        'book_list': book_list
+    }
+    # return HttpResponse(book_list) #after using templates, we no longer need HttpReponse, but rather render
+    return render(request, 'myapp/index.html', context)
+
+
+# def products(request):
+#     return HttpResponse('Products') #no longer needed as we'll be displaying detailed views
+
+def detail(request,book_id):
+    # book_list = Book.objects.all()
+    # mybook = book_list[book_id-1]
+    # return HttpResponse("This is book number %s<br/>Price: $%s<br/>Book Name: %s<br/>Book Description: %s " % (book_id, mybook.price, mybook.name, mybook.desc))
+
+    book = Book.objects.get(id=book_id)
+    return render(request,'myapp/detail.html', {'book': book})
