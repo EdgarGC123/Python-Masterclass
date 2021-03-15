98e2defe2369552d119fb71b95c446e86fe9bea2
Edgar GC <Edgar.GalvanCuesta@gmail.com>
2021-03-15 16:33:20 -0500

    added update and edit functionality to our django app

--- sec20-django/demo/mysite/myapp/urls.py
+++ sec20-django/demo/mysite/myapp/urls.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from django.urls import path
 from myapp import views
@@ -9,4 +8,6 @@ urlpatterns = [
     # path('products/', views.products, name="products")# products is removed from my app views. 
     #book/1 detail views
     path('book/<int:book_id>/', views.detail, name='detail'),
-]
+    path('add/', views.add_book, name="add_book"),
+    path('update/<int:id>/',views.update,name='update'),
+] 
--- sec20-django/demo/mysite/myapp/views.py
+++ sec20-django/demo/mysite/myapp/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import Book #importing our books class
 # Create your views here.
+
+from .forms import BookForm
 
 def index(request):
     book_list = Book.objects.all() #getting all the list of books
@@ -23,3 +25,24 @@ def detail(request,book_id):
 
     book = Book.objects.get(id=book_id)
     return render(request,'myapp/detail.html', {'book': book})
+
+def add_book(request):
+    if request.method == 'POST':
+        # book_details = request.POST.get('name','desc','price')
+        # print(book_details)
+        name = request.POST.get('name',)
+        desc = request.POST.get('desc',)
+        price = request.POST.get('price',)
+        bi = request.FILES['book_image']
+
+        book = Book(name = name, desc = desc, price = price, book_image = bi)
+        book.save()
+    return render(request, 'myapp/add_book.html')
+
+def update(request, id):
+    book = Book.objects.get(id=id)
+    form = BookForm(request.POST or None, request.FILES, instance=book)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    return render(request, 'myapp/edit.html', {'form':form, 'book':book})
